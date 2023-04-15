ed1e0edaabcf0794a4954a5aafb4ad9a23d282e3
saonimustafi <saonimustafi1994@gmail.com>
2023-04-15 14:36:49 -0700

    Show request book/delete request after login

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -2,20 +2,66 @@ import { useState, useRef, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import Book from '../components/Book';
 import TopNav from '../components/TopNav';
-// import { books } from '../data/books';
 import './HomePage.css';
 
 function HomePage() {
   const [searchTerm, setSearchTerm] = useState('');
   const [expanded, setExpanded] = useState(false);
   const [books, setBooks] = useState([]);
+  const [requestedBookIDs, setRequestedBookIDs] = useState([]);
+  const [isFetchRequestedBooksCompleted, setIsFetchRequestedBooksCompleted] = useState(false);
+  const [currentUser, setCurrentUser] = useState([]);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
   const searchRef = useRef(null);
-  // const navigate = useNavigate();
+  const navigate = useNavigate()
+
+  useEffect(() => {        
+    async function checkLoggedInAndFetchUserRequests() {
+        const token = localStorage.getItem('token')
+        if (token) {
+            const req = await fetch('http://127.0.0.1:3000/checkLoggedIn', {
+                headers: {
+                    'x-access-token': token,
+                },
+            })
+            const data = await req.json()
+            if (data.status === 'ok') {
+                setIsLoggedIn(true)
+                setCurrentUser(data)
+                
+                try {
+                    const response = await fetch(`http://localhost:3000/requests/${data.id}`)
+                    const responseData = await response.json()
+                    const bookIDs = (responseData && responseData.books) ? responseData.books.map((request) => request.book_id) : [];                    
+                    setRequestedBookIDs(bookIDs)
+                    setIsFetchRequestedBooksCompleted(true)
+                }
+                catch(error) {
+                    console.error(error);
+                }
+                
+            } else {
+                setIsLoggedIn(false)
+                setCurrentUser({})
+                console.log('Error occurred during user authentication: ' + data.error)
+                navigate('/')
+            }
+        } 
+        else {
+            setIsLoggedIn(false)
+            setCurrentUser({})
+            navigate('/')
+        }
+    }
+    
+    checkLoggedInAndFetchUserRequests();
+}, []);
+
+
 
   const handleSearch = async (event) => {
     event.preventDefault();
     if (searchTerm.trim()) {
-      // navigate(`/search/${searchTerm}`);
       try {
         const response = await fetch(`http://127.0.0.1:3000/search?search_query=${searchTerm}`);
         const data = await response.json();
@@ -37,8 +83,8 @@ function HomePage() {
     async function fetchInitialData() {
       try {
         const response = await fetch(`http://127.0.0.1:3000/search?search_query=`);
-        const data = await response.json();
-        setBooks(data);
+        const allBooks = await response.json();
+        setBooks(allBooks);
       } catch (error) {
         console.log('Error:', error);
       }
@@ -65,10 +111,45 @@ function HomePage() {
           <button type="submit">Search</button>
         </form>
       </div>
+
       <div className="book-grid">
-        {books.map((book) => (
-          <Book key={book.id} book={book} />
-        ))}
+        {isLoggedIn ? (
+          !isFetchRequestedBooksCompleted ? (
+            <p>Loading...</p>
+          ) : (
+            <>
+              {console.log("In HomePage.js: requestedBookIDs=" + requestedBookIDs)}
+              {books.map((book) => {
+                const isBookRequested = requestedBookIDs.includes(book.id);
+                {console.log("In HomePage.js: book=" + book.title + " id="+book.id+" isBookRequested="+isBookRequested)}
+                return (
+                  <Book
+                    key={book.id}
+                    book={book}
+                    user_id={currentUser.id}
+                    isBookRequested={isBookRequested}
+                    isLoggedIn={isLoggedIn}
+                  />
+                );
+              })}
+            </>
+          )
+        ) : 
+        (
+          <>
+            {books.map((book) => {
+              return (
+                <Book
+                  key={book.id}
+                  book={book}
+                  user_id={currentUser.id}
+                  isLoggedIn={isLoggedIn}
+                  isFetchRequestedBooksCompleted={isFetchRequestedBooksCompleted}
+                />
+              );
+            })}
+          </>
+        )}
       </div>
     </div>
   );
